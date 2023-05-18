b43df094aff3090a7d1cbd2e02d278f7ed208022
Lucas Neuber <Lucas-Juergen.Neuber@student.tu-freiberg.de>
2023-05-18 15:39:08 +0200

    Move to color palette

--- MandelbrotSharp/Mandelbrot.cs
+++ MandelbrotSharp/Mandelbrot.cs
@@ -6,11 +6,21 @@ public class Mandelbrot
 {
 
     public static readonly int MAX_ITERATIONS = 1000;
+    private Rgba32[] colors = new Rgba32[MAX_ITERATIONS];
+
+    private void initPalette() {
+        for (int i = 0; i < MAX_ITERATIONS; i++) {
+            var color = new Hsl(((i / 100.0f) * 360.0f), 1.0f, 0.5f);
+            var rgb = ColorSpaceConverter.ToRgb(color);
+            colors[i] = new Rgba32(rgb.R, rgb.G, rgb.B, 1.0f);
+        }
+    }
 
     public void generateImage(int height)
     {
         int width = (int)(height * 1.5);
         Image<Rgba32> image = new Image<Rgba32>(width, height);
+        initPalette();
 
         Parallel.For(0, width, x =>
         {
@@ -21,11 +31,7 @@ public class Mandelbrot
                 // Scale y to (-1, 1), zero is the center
                 double imaginary = (y - height / 2.0) / (height / 2.0);
                 int iterations = mandelbrot(new Complex(real, imaginary));
-
-                var color = new Hsl(((iterations / 100.0f) * 360.0f), 1.0f, iterations < MAX_ITERATIONS ? 0.5f : 0);
-                var rgb = ColorSpaceConverter.ToRgb(color);
-
-                image[x, y] = new Rgba32(rgb.R, rgb.G, rgb.B);
+                image[x, y] = iterations < MAX_ITERATIONS ? colors[iterations] : new Rgba32(0, 0, 0, 1.0f);
             }
         });
 
